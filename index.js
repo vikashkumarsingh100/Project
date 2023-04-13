eabd8f4b76676f8fff5d48220d62b99111349df9
vikashsinghmps <vikash.singh@mpslimited.com>
2023-04-13 13:21:30 +0530

    add layout views staticfiles

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+var expressLayouts = require('express-ejs-layouts');
+app.use(expressLayouts)
+app.use(express.static('./static'))
+app.set('layout extractStyles',true);
 //var ejs = require("ejs");
 var routers = require("./routers/app.js");
+// app.set('layout','layout')
+
 app.use('/',routers);
 app.set("view engine", "ejs");
+app.set('views','./views')
+
 app.listen(3000, function (err) {
   if (err) console.log(err);
   console.log("app is running");
